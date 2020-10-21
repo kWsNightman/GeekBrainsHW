428bcc2e2e5a11571639e6368e9491065ad4c768
MirukAlex <kws.nightmen@gmail.com>
2020-10-21 14:10:33 +0300

    Update 5_4

--- HomeWork_5_4.py
+++ HomeWork_5_4.py
@@ -1,11 +1,16 @@
-numbers_rus = {'1': 'Один', '2': 'Два', '3': 'Три', '4': 'Четыре'}
+from googletrans import Translator
+
+trans = Translator()
+
 with open('text_4.txt', 'r', encoding='utf-8') as file:
     text = [i.split() for i in file.readlines()]
 
 for i in text:
-    i.insert(0, numbers_rus.get(i[2]))
+    result = trans.translate(i[0], src='en', dest='ru')
+    i.insert(0, result.text)
     i.remove(i[1])
 
 with open('text_4_1.txt', 'w', encoding='utf-8') as file_save:
     text = [' '.join(i) + '\n' for i in text]
     file_save.writelines(text)
+print(text)
